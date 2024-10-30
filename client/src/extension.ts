de899719e74a3e0cd258cd05cbeb37618bccbda6
Jesse Bouwman <jlbouwman@hrl.com>
2024-10-30 12:25:02 -0700

    file assocation, connect to local server

---
+++ client/src/extension.ts
@@ -0,0 +1,50 @@
+import * as net from 'net';
+import { ExtensionContext } from 'vscode';
+
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	StreamInfo
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
+
+export function activate(_context: ExtensionContext) {
+
+	// TODO this is set up to connect to a local copy of coalton-lsp.
+	// It ought to be shipped with lsp server binaries at some point.
+
+	const connectionInfo = {
+		port: 7887,
+		host: "127.0.0.1"
+	};
+	const serverOptions = () => {
+		// Connect to language server via socket
+		const socket = net.connect(connectionInfo);
+		const result: StreamInfo = {
+			writer: socket,
+			reader: socket
+		};
+		return Promise.resolve(result);
+	};
+
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: [{ scheme: 'file', language: 'coalton' }]
+	};
+
+	client = new LanguageClient(
+		'coalton',
+		'Coalton',
+		serverOptions,
+		clientOptions
+	);
+
+	client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
--- src/extension.ts
+++
@@ -1,9 +0,0 @@
-import * as vscode from 'vscode';
-import { LanguageClient } from 'vscode-languageclient';
-
-export function activate(context: vscode.ExtensionContext) {
-    console.log('activate coalton extension');
-}
-
-export function deactivate() {}
-
